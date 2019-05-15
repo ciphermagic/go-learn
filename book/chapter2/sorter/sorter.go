bb4daf6d42dee380e691a44d68cb740aef7478e3
ciphermagic <ciphermagic@yeah.net>
2019-05-15 22:30:24 +0800

    排序

--- book/chapter2/sorter/sorter.go
+++ book/chapter2/sorter/sorter.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
+	"os"
+	"strconv"
 )
 
 var infile *string = flag.String("i", "infile", "File contains values for sorting")
@@ -13,5 +17,48 @@ func main() {
 	flag.Parse()
 	if infile != nil {
 		fmt.Println("infile =", *infile, "outfile =", *outfile, "algorithm =", *algorithm)
+		values, err := readValues(*infile)
+		if err == nil {
+			fmt.Println("Read values:", values)
+		} else {
+			fmt.Println(err)
+		}
 	}
+}
+
+func readValues(infile string) (values []int, err error) {
+	file, err := os.Open(infile)
+	if err != nil {
+		fmt.Println("Failed to open the input file ", infile)
+		return
+	}
+	defer file.Close()
+
+	br := bufio.NewReader(file)
+	values = make([]int, 0)
+
+	for {
+		line, isPrefix, err1 := br.ReadLine()
+		if err1 != nil {
+			if err1 != io.EOF {
+				err = err1
+			}
+			break
+		}
+
+		if isPrefix {
+			fmt.Println("A too long line, seems unexpected.")
+			return
+		}
+
+		str := string(line)
+		value, err1 := strconv.Atoi(str)
+		if err1 != nil {
+			err = err1
+			return
+		}
+
+		values = append(values, value)
+	}
+	return
 }
