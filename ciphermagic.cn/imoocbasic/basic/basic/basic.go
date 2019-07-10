fbc46020cf38f4d809f6b50c04a40c105b062764
ciphermagic <ciphermagic@yeah.net>
2019-07-10 22:27:58 +0800

    testing

--- ciphermagic.cn/imoocbasic/basic/basic/basic.go
+++ ciphermagic.cn/imoocbasic/basic/basic/basic.go
@@ -41,9 +41,11 @@ func euler() {
 
 func triangle() {
 	var a, b int = 3, 4
-	var c int
-	c = int(math.Sqrt(float64(a*a + b*b)))
-	fmt.Println(c)
+	fmt.Println(calcTriangle(a, b))
+}
+
+func calcTriangle(a, b int) int {
+	return int(math.Sqrt(float64(a*a + b*b)))
 }
 
 func consts() {
---
+++ ciphermagic.cn/imoocbasic/container/nonrepeating/nonrepeating_test.go
@@ -0,0 +1,30 @@
+package main
+
+import "testing"
+
+func TestSubstr(t *testing.T) {
+	tests := []struct {
+		s   string
+		ans int
+	}{
+		// Normal cases
+		{"abcabcbb", 3},
+		{"pwwkew", 3},
+
+		// Edge cases
+		{"", 0},
+		{"b", 1},
+		{"bbbbb", 1},
+		{"abcabcabcd", 4},
+
+		// Chinese support
+		{"一二三二一", 3},
+	}
+
+	for _, tt := range tests {
+		actual := lengthOfNonRepeatingSubStr(tt.s)
+		if actual != tt.ans {
+			t.Errorf("lengthOfNonRepeatingSubStr('%s'); got %d; expected %d", tt.s, actual, tt.ans)
+		}
+	}
+}
