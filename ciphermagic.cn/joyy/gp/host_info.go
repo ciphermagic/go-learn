b518fa6b5ba3443378f0143c4d598f4b395407d8
cuishunen <cuishunen@joyy.com>
2023-03-20 23:10:39 +0800

    get host info

--- ciphermagic.cn/joyy/gp/host_info.go
+++ ciphermagic.cn/joyy/gp/host_info.go
@@ -4,32 +4,25 @@ import (
 	"encoding/json"
 	"os"
 	"strings"
-	"sync"
 )
+
+var HostInfo hostInfo
 
 type hostInfo struct {
 	Idc  string `json:"idc"`
 	Name string `json:"name"`
 }
 
-var HostInfo hostInfo
-var once = sync.Once{}
-
 func init() {
-	once.Do(func() {
-		const filename = "/Users/cipher/Downloads/host.ini"
-		if bytes, err := os.ReadFile(filename); err == nil {
-			m := make(map[string]string)
-			str := string(bytes)
-			lines := strings.Split(str, "\n")
-			for _, line := range lines {
-				if len(line) > 0 {
-					info := strings.Split(line, "=")
-					m[info[0]] = info[1]
-				}
+	const filename = "/Users/cipher/Downloads/host.ini"
+	if b, e := os.ReadFile(filename); e == nil {
+		m := make(map[string]string)
+		for _, line := range strings.Split(string(b), "\n") {
+			if info := strings.Split(line, "="); len(info) == 2 {
+				m[strings.TrimSpace(info[0])] = strings.TrimSpace(info[1])
 			}
-			marshal, _ := json.Marshal(m)
-			json.Unmarshal(marshal, &HostInfo)
 		}
-	})
+		marshal, _ := json.Marshal(m)
+		json.Unmarshal(marshal, &HostInfo)
+	}
 }
